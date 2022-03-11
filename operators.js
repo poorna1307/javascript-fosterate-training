a289b9aa56f7c7c143bca0930429658b2678bd7a
poornachander <poornachander.thumu13@gmail.com>
2022-03-11 19:54:13 +0530

    operators.js file created

---
+++ operators.js
@@ -0,0 +1,30 @@
+
+console.log("Operators.js Program")
+/*Operators in javascript are:
+1)Arithmetic Operators
+2)Assignment Operators
+3)String Operators
+4)Comparision Operators
+5)Logical Operators
+6)Type Operators
+7)Bitwise Operators
+*/
+//code for Arithmetic Operators
+console.log("Code For Arithmetic Operators")
+let a=10
+let b=2
+console.log("a+b= "+(a+b))
+console.log("a-b= "+(a-b))
+console.log("a**b= "+(a**b))
+console.log("a/b= "+(a/b))
+console.log("Code For Assignment Operators")
+let x=10 // "=" Operator is used to assgin values to variables
+let y=5
+console.log("x+=y:"+(x+=y))
+console.log("x**=y:"+(x**=y))
+console.log("Code For String Operators")
+let first_name="Poorna Chander"
+let last_name="Thumu"
+console.log(first_name+" "+last_name)
+
+//These are the some of the operators used in Javascript commanly.
