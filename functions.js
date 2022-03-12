499cc9734c677619efe09ba6799f6cbba5e05378
poornachander <poornachander.thumu13@gmail.com>
2022-03-12 20:14:41 +0530

    functions.js file created

---
+++ functions.js
@@ -0,0 +1,8 @@
+//Program on functions in javascript
+//For defining a function we need to use function keyword followed by function name
+console.log("Program on functions in javascript")
+function add(a,b){
+    return a+b
+}
+let s=add(10,20)
+console.log(s)
