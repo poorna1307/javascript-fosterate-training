e19be818f5c4f43ba5402ad61970f83f2d5b375f
poornachander <poornachander.thumu13@gmail.com>
2022-03-12 19:55:34 +0530

    datatypes.js file created

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+console.log("Program on JavaScript Datatypes:")
+//Datatypes in javascript are 2 types - 1)primitive and 2)non-primitive
+//numbers,string,null,Boolean and Symbol comes under primitive datatypes of java
+//objects comes under non-primitive types because objects can store different types data
+
+//number
+let a1=100
+console.log(a1)
+//BigInt
+let b1=10000000000000000000000000n
+console.log(b1)
+//string
+let c1="Poorna"
+console.log(c1)
+//null
+let d1=null
+console.log(d1)
+//boolean
+let e1=true
+console.log(e1)
+
+
+//typeof operator is used to check the type of a particular variable
+console.log("Typeof e1 is: ",typeof(e1))
+console.log("Typeof b1 is: ",typeof(b1))
