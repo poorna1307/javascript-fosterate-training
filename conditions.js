6e0c06d26529c029773517d42839b0f55c746d29
poornachander <poornachander.thumu13@gmail.com>
2022-03-14 23:31:26 +0530

    conditions.js file created

---
+++ conditions.js
@@ -0,0 +1,41 @@
+//program on conditional statement
+console.log("Program on conditonal statement:")
+let num=10
+if(num==10){
+    console.log("num equals to 10")
+}
+let age=17
+if(a>=18){
+    console.log("Your eligible for voting")
+}
+else{
+    console.log("Your not eligible for voting")
+}
+let num1=1
+if(num1==1){
+    console.log("num1 equal to one")
+}
+else if(num1==2){
+    console.log("num1 equal to two")
+}
+else{
+    console.log("num 1 is greater than 2")
+}
+
+console.log("switch statement")
+
+let num2=2
+switch(num2){
+    case 1:
+        console.log("Num2 is equal to one")
+        break
+    case 2:
+        console.log("Num2 is equal to two")
+        break
+    case 3:
+        console.log("Num3 is equal to three")
+        break
+    default:
+        console.log("The number is greater than 3")
+        break
+}
