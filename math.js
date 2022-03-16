ee6843855b6151c83807e4380c0afa1b7e6ac837
poornachander <poornachander.thumu13@gmail.com>
2022-03-16 21:34:11 +0530

    math.js file created

---
+++ math.js
@@ -0,0 +1,7 @@
+console.log("Program on math objects")
+pi=Math.PI
+console.log("PI value is:"+pi)
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.pow(8,2))
+console.log(Math.sqrt(49))
