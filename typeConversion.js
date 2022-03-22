30c66bac21d2f32ef12cb165bb341ac114ee75de
poornachander <poornachander.thumu13@gmail.com>
2022-03-22 19:30:42 +0530

    typeconversion.js file created

---
+++ typeConversion.js
@@ -0,0 +1,17 @@
+console.log("String to number converstion")
+let tc=Number("3.14") 
+console.log(tc)
+console.log("Converting number to string:")
+let tc1=String(10)
+console.log(tc1)
+console.log("Converting date to number:")
+dd = new Date();
+console.log(dd)
+console.log(Number(dd))
+console.log("Converting date to string:")
+dd1=new Date()
+console.log(String(dd1))
+console.log("Converting bool to decimal")
+console.log(Number(false))
+console.log(Number(true))
+
