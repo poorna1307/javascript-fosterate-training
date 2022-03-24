3624c9e4a7a1476a6abf570574bf6a4b222d75c3
poornachander <poornachander.thumu13@gmail.com>
2022-03-24 22:49:03 +0530

    this.js file created

---
+++ this.js
@@ -0,0 +1,9 @@
+const person_this = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+console.log(person_this.fullName())
